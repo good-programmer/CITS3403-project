bccb4896d81f0fca347b1ee6d1d462b8cfbf9c60
Thomas Nguyen <tnguyen7523@gmail.com>
2024-04-19 08:53:28 +0800

    Added unit tests for account validation and creation. Added unit tests for database integrity checks for User and Follow. Added some relationships to orm.

--- project/blueprints/models.py
+++ project/blueprints/models.py
@@ -6,8 +6,8 @@ db = SQLAlchemy()
 class User(UserMixin, db.Model):
     __tablename__ = 'Users'
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(1000), unique=True)
     password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
 
     def follow_user(self, user):
         follow = Follow(userID=user.id, followerID=self.id)
@@ -29,7 +29,7 @@ class Puzzle(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(1000))
     authorID = db.Column(db.Integer, db.ForeignKey('Users.id'))
-    author = db.relationship("User", backref=db.backref("puzzles", uselist=False))
+    author = db.relationship("User", foreign_keys=[authorID], backref=db.backref("puzzles"))
     dateCreated = db.Column(db.DateTime)
     content = db.Column(db.Text)
 
@@ -44,6 +44,8 @@ class LeaderboardRecord(db.Model):
     __tablename__ = "Leaderboard"
     userID = db.Column(db.Integer, db.ForeignKey('Users.id'), primary_key=True)
     puzzleID = db.Column(db.Integer, db.ForeignKey('Puzzles.id'), primary_key=True)
+    puzzle = db.relationship("Puzzle", foreign_keys=[puzzleID], backref=db.backref("scores"))
+    user = db.relationship("User", foreign_keys=[userID], backref=db.backref("scores"))
     score = db.Column(db.Integer)
     dateSubmitted = db.Column(db.DateTime)
 
