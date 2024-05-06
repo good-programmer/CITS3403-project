2c2a04cfcd628810c3bd57ea95aeec46f0de8cda
Thomas Nguyen <tnguyen7523@gmail.com>
2024-05-07 03:09:29 +0800

    Added rate slider on puzzle info page. TBA: POST request to server on click

--- project/static/js/puzzleinfo.js
+++ project/static/js/puzzleinfo.js
@@ -1,4 +1,5 @@
-window.onload = function() {
+window.onload = async function() {
+    let puzzleid = window.location.pathname.split('/')[2]
     let switchLeaderboard = document.getElementById("switch-leaderboard-button");
     let main = document.getElementById("main-leaderboard");
     let following = document.getElementById("following-leaderboard");
@@ -13,4 +14,28 @@ window.onload = function() {
             following.style.display = "block";
         }
     }
+
+    let storedRating = 0;
+    let currentRating = 0;
+    let rateSlider = document.getElementById('rate-slider');
+    let x = rateSlider.getBoundingClientRect().left
+    let w = document.getElementById('rate-back').getBoundingClientRect().right - x
+    rateSlider.addEventListener('mousemove',function(e) {
+        let offset = (e.x - x) / w;
+        offset = Math.round(offset * 10) / 10 * w
+        document.getElementById('rate-current').setAttribute('style','width:'+offset+'px');
+        currentRating = Math.min(5, Math.max(0, offset / w * 5));
+
+    })
+    function resetRating(e) {
+        document.getElementById('rate-current').setAttribute('style','width:'+ storedRating / 5 * w+'px');
+    }
+    rateSlider.addEventListener('mouseleave', resetRating);
+
+    let response = await fetch("/puzzle/" + puzzleid);
+    let puzzleInfo = await response.json();
+    storedRating = 'rated' in puzzleInfo ? puzzleInfo['rated']['rating'] : 0
+    resetRating();
+    console.log(puzzleInfo);
+
 }
