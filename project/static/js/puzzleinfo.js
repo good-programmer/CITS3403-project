3d855d08655f86be42331ffde26235fddb5499ef
Thomas Nguyen <tnguyen7523@gmail.com>
2024-05-07 03:28:46 +0800

    Rate slider now greys out when a user isn't able to rate (they haven't completed the puzzle).

--- project/static/js/puzzleinfo.js
+++ project/static/js/puzzleinfo.js
@@ -15,49 +15,53 @@ window.onload = async function() {
         }
     }
 
-    let storedRating = 0;
-    let currentRating = 0;
+    let response = await fetch("/puzzle/" + puzzleid);
+    let puzzleInfo = await response.json();
+    let storedRating = 'rated' in puzzleInfo ? puzzleInfo['rated']['rating'] : 0
     let rateSlider = document.getElementById('rate-slider');
     let x = rateSlider.getBoundingClientRect().left
     let w = document.getElementById('rate-back').getBoundingClientRect().right - x
-    rateSlider.addEventListener('mousemove',function(e) {
-        let offset = (e.x - x) / w;
-        offset = Math.round(offset * 10) / 10 * w
-        document.getElementById('rate-current').setAttribute('style','width:'+offset+'px');
-        currentRating = Math.min(5, Math.max(0, offset / w * 5));
 
-    })
     function resetRating(e) {
         document.getElementById('rate-current').setAttribute('style','width:'+ storedRating / 5 * w+'px');
     }
     rateSlider.addEventListener('mouseleave', resetRating);
-
-    let debounce = false;
-    rateSlider.addEventListener('click', async function(){
-        if (debounce) return;
-        debounce = true;
-        let rating = currentRating;
-        let action = '/puzzle/' + puzzleid + '/rate';
-        const response = await fetch(action, {
-            method: "POST",
-            mode: "same-origin",
-            cache: "no-cache",
-            credentials: "same-origin",
-            headers: {"Content-Type": "application/json"},
-            redirect: "error",
-            body: JSON.stringify({"rating": parseFloat(rating)}),
-            });
-        if (response.status === 200) {
-            storedRating = rating;
-            resetRating();
-        }
-        debounce=false;
-    });
-
-    let response = await fetch("/puzzle/" + puzzleid);
-    let puzzleInfo = await response.json();
-    storedRating = 'rated' in puzzleInfo ? puzzleInfo['rated']['rating'] : 0
     resetRating();
-    console.log(puzzleInfo);
 
+    if ('score' in puzzleInfo) {
+        let currentRating = 0;
+        rateSlider.addEventListener('mousemove',function(e) {
+            let offset = (e.x - x) / w;
+            offset = Math.round(offset * 10) / 10 * w
+            document.getElementById('rate-current').setAttribute('style','width:'+offset+'px');
+            currentRating = Math.min(5, Math.max(0, offset / w * 5));
+
+        })
+
+        let debounce = false;
+        rateSlider.addEventListener('click', async function(){
+            if (debounce) return;
+            debounce = true;
+            let rating = currentRating;
+            let action = '/puzzle/' + puzzleid + '/rate';
+            const response = await fetch(action, {
+                method: "POST",
+                mode: "same-origin",
+                cache: "no-cache",
+                credentials: "same-origin",
+                headers: {"Content-Type": "application/json"},
+                redirect: "error",
+                body: JSON.stringify({"rating": parseFloat(rating)}),
+                });
+            if (response.status === 200) {
+                storedRating = rating;
+                resetRating();
+            }
+            debounce=false;
+        });
+    } else {
+        rateSlider.classList += ' disabled';
+        rateSlider.style.color = 'darkgrey';
+        rateSlider.style.opacity = '0.6';
+    }
 }
