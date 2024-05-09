1520bd8622439814e91dfd3a0b220565215f386b
naetnaetnaet <buluran.nathan@gmail.com>
2024-05-09 15:59:12 +0800

    Updated landing page navbar to accomodate new routes,updated landing page title to 'word-amble'and added new feature - allows users to view the 10 most recent puzzles created, also allowing users to navigate to the puzzles via the puzzle title or id as well as allowing users to navigate to creator profiles via the puzzles creator name or id

--- project/static/js/index.js
+++ project/static/js/index.js
@@ -1,15 +1,43 @@
+const postTemplate = document.querySelector("[post-template]")
+const postContainer = document.querySelector("[data-post-container]")
 
+fetch('/puzzle/search/recent')
+    .then(response => response.json())
+    .then(data => {
+        data.puzzles.forEach(puz => {
+            const post = postTemplate.content.cloneNode(true)
 
-window.onload = function(){
-    console.log('yes');
-    const puzTable = document.getElementById("puz-table");
+            const puzTitle = post.querySelector("[data-puz-title]")
+            puzTitle.textContent = puz.title
+            puzTitle.href = '/puzzle/'+String(puz.id)+'/play'
 
-    const puzzle = document.createElement("div");
+            const puzID = post.querySelector("[data-puz-id]")
+            puzID.textContent = puz.id
+            puzID.href = '/puzzle/'+String(puz.id)+'/play'
 
-    puzzle.classList.add("post");
-    puzzle.textContent = "yes";
+            const authName = post.querySelector("[data-author-name]")
+            authName.textContent = puz.creator
+            authName.href = '/user/'+String(puz.creatorID)+'/profile'
 
-    puzTable.append(puzzle)
+            const authID = post.querySelector("[data-author-id]")
+            authID.textContent = puz.creatorID
+            authID.href = '/user/'+String(puz.creatorID)+'/profile'
 
-}
+            const dateCreation = post.querySelector("[data-date-created]")
+            dateCreation.textContent = puz.dateCreated
+
+            const playcount = post.querySelector("[data-playcount]")
+            playcount.textContent = puz.play_count
+
+            const avgRating= post.querySelector("[data-avg-rating]")
+            avgRating.textContent = puz.average_rating
+
+            const highestScore = post.querySelector("[data-highscore]")
+            highestScore.textContent = puz.highscore
+
+            postContainer.append(post)
+        });
+    })
+    .catch(error => console.error('Error fetching data:', error));
+
 
