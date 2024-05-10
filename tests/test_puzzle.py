c9796ce5b31918a65ae4b608df06009dea1f5ddd
Thomas Nguyen <tnguyen7523@gmail.com>
2024-05-11 01:22:59 +0800

    Separated test_db into test_user and test_puzzle. Updated test_user to include password tests.

--- tests/test_puzzle.py
+++ tests/test_puzzle.py
@@ -1,3 +1,6 @@
+'''
+Test ORM and methods of Puzzle class
+'''
 from datetime import datetime
 import unittest
 
@@ -5,74 +8,9 @@ from sqlalchemy import exc
 
 from tests import TestObject, app
 
-from project.blueprints.models import db, User, Follow, Puzzle, LeaderboardRecord, Rating
+from project.blueprints.models import db, Puzzle, LeaderboardRecord, Rating
 
 from project.utils import user_utils, puzzle_utils
-
-
-class UserModelCase(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls) -> None:
-        cls.app_context = app.app_context()
-        cls.app_context.push()
-        cls.t = TestObject(app, db)
-        return super().setUpClass()
-
-    def tearDown(self):
-        self.t.clear_db()
-    
-    @classmethod
-    def tearDownClass(cls) -> None:
-        cls.app_context.pop()
-        return super().tearDownClass()
-
-    def test_name_unique(self):
-        '''
-        Tests that only distinct usernames can exist in the User table
-        '''
-        user_utils.add_user("MAIN_USER", "132131")
-        self.assertRaises(exc.IntegrityError, user_utils.add_user, "MAIN_USER", "789")
-
-    def test_follow_integrity(self):
-        '''
-        Tests the integrity of the database (in particular, the unique constraint).
-        \nTests that the follow and unfollow methods of User work as expected.
-        '''
-        current_user = user_utils.add_user("MAIN_USER", "132131")
-        u2 = user_utils.add_user("TEST2", "123")
-        u2.follow_user(current_user)
-
-        self.assertRaises(exc.IntegrityError, u2.follow_user, current_user)
-        db.session.rollback()
-        self.assertTrue(u2.unfollow_user(current_user))
-        self.assertFalse(u2.unfollow_user(current_user))
-
-    def test_follow(self):
-        '''
-        Tests that the relationship between User and Follow ORMs is correct and appends followers correctly.
-        \nTests the is_following method of User
-        '''
-        current_user = user_utils.add_user("MAIN_USER", "132131")
-        
-        for i in range(20):
-            user_utils.add_user("TEST_FOLLOWER" + str(i), "123").follow_user(current_user)
-
-        f1 = db.session.query(Follow).filter_by(userID=current_user.id).all()
-        f2 = db.session.query(User).filter_by(id=current_user.id).first().followers
-        x = [f.follower.name for f in f1]
-        y = [f.follower.name for f in f2]
-        x.sort()
-        y.sort()
-
-        self.assertListEqual(x, y)
-
-        u2 = user_utils.add_user("TEST2", "123")
-        self.assertFalse(u2.is_following(current_user))
-        u2.follow_user(current_user)
-        self.assertTrue(u2.is_following(current_user))
-        u2.unfollow_user(current_user)
-        self.assertFalse(u2.is_following(current_user))
-        self.assertIsNone(db.session.query(Follow).filter_by(userID=current_user.id, followerID=u2.id).first())
 
 class PuzzleModelCase(unittest.TestCase):
     @classmethod
@@ -192,6 +130,3 @@ class PuzzleModelCase(unittest.TestCase):
         puzzle.add_rating(user, 5)
         avg1 = (r + 5) / (l + 1)
         self.assertEqual(avg1, puzzle.average_rating)
-
-if __name__ == '__main__':
-    unittest.main(verbosity=2)
---
+++ tests/test_user.py
@@ -0,0 +1,71 @@
+import unittest
+
+from sqlalchemy import exc
+
+from tests import TestObject, app
+
+from project.blueprints.models import db, User, Follow
+
+from project.utils import user_utils
+
+class UserModelCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.app_context = app.app_context()
+        cls.app_context.push()
+        cls.t = TestObject(app, db)
+        return super().setUpClass()
+
+    def tearDown(self):
+        self.t.clear_db()
+    
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.app_context.pop()
+        return super().tearDownClass()
+    
+    def test_password(self):
+        current_user = user_utils.add_user("MAIN_USER", "132131")
+        current_user.set_password("hello")
+
+        self.assertTrue(current_user.check_password("hello"))
+        self.assertFalse(current_user.check_password("bye"))
+        self.assertFalse(current_user.check_password(""))
+
+    def test_name_unique(self):
+        '''
+        Tests that only distinct usernames can exist in the User table
+        '''
+        user_utils.add_user("MAIN_USER", "132131")
+        self.assertRaises(exc.IntegrityError, user_utils.add_user, "MAIN_USER", "789")
+        db.session.rollback()
+
+    def test_follow(self):
+        '''
+        Tests that the relationship between User and Follow ORMs is correct and appends followers correctly.
+        \nTests the is_following method of User
+        '''
+        current_user = user_utils.add_user("MAIN_USER", "132131")
+        
+        for i in range(5):
+            user_utils.add_user("TEST_FOLLOWER" + str(i), "123").follow_user(current_user)
+
+        f1 = db.session.query(Follow).filter_by(userID=current_user.id).all()
+        f2 = db.session.query(User).filter_by(id=current_user.id).first().followers
+        x = [f.follower.name for f in f1]
+        y = [f.follower.name for f in f2]
+        x.sort()
+        y.sort()
+
+        self.assertListEqual(x, y)
+
+        u2 = user_utils.add_user("TEST2", "123")
+        self.assertFalse(u2.is_following(current_user))
+        u2.follow_user(current_user)
+        self.assertTrue(u2.is_following(current_user))
+        self.assertRaises(exc.IntegrityError, u2.follow_user, current_user)
+        db.session.rollback()
+        self.assertTrue(u2.unfollow_user(current_user))
+        self.assertFalse(u2.unfollow_user(current_user))
+        self.assertFalse(u2.is_following(current_user))
+        self.assertIsNone(db.session.query(Follow).filter_by(userID=current_user.id, followerID=u2.id).first())
