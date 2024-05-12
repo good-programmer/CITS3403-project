2b6f9ec16837405d1501c72831aabed8217285a8
Thomas Nguyen <tnguyen7523@gmail.com>
2024-05-12 20:16:42 +0800

    Target url displayed in navbar for effect. Error msg on login/register page now red to distinguish it

---
+++ project/static/js/navbar.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+    let nav = document.getElementById("nav-location");
+    let links = document.querySelectorAll(".header > a")
+    for (let link of links){
+        link.addEventListener('mousemove', function(){
+            nav.textContent = ">>>"+link.getAttribute("href").match(/\/.*/g)[0]
+        });
+        link.addEventListener('mouseleave', function() {
+            nav.textContent = ">>>"
+        })
+    }
+}
