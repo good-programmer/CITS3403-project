3fa2597b3ee24c093944f151e7ff1cdb9b03ab46
naetnaetnaet <buluran.nathan@gmail.com>
2024-05-15 02:24:52 +0800

    removed comments and indents in search.js and changed class = dropdown to class = rating which were orginally intended to replicable to being specific to browse by rating, added button labels for each way to browse by, which hides and shows input fields based on whether or not the button is toggled and added browse by playcount input fields as well as sanitisation for them ensuring the safe max int is not reached, only numbers are allowed as inputs(including e and negatives' and max is always greater than min

--- project/static/js/search.js
+++ project/static/js/search.js
@@ -1,49 +1,94 @@
 const minRating = document.getElementById('min-rating');
 const maxRating = document.getElementById('max-rating');
 
-    // Event listener for menu1
-    minRating.addEventListener('change', function() {
-        const currentMinValue = parseInt(minRating.value);
-        const previousMaxValue = maxRating.value;
+//event listeners for rating
+minRating.addEventListener('change', function() {
+    const currentMinValue = parseInt(minRating.value);
+    const previousMaxValue = maxRating.value;
 
-        // Clear existing options in menu2
-        maxRating.innerHTML = '';
+    maxRating.innerHTML = '';
 
+    const optionElement = document.createElement('option');
+    optionElement.textContent = 'Maximum Rating:';
+    optionElement.value = 5;
+    maxRating.appendChild(optionElement);
+
+    for (let i = currentMinValue; i <= 5; i++) {
         const optionElement = document.createElement('option');
-        optionElement.textContent = 'Maximum Rating:';
-        optionElement.value = 5;
+        optionElement.textContent = i;
+        optionElement.value = i;
         maxRating.appendChild(optionElement);
+    }
+    if (parseInt(previousMaxValue) >= currentMinValue) {
+        maxRating.value = previousMaxValue;
+    } 
+});
 
-        // Add new options to menu2 based on selection of menu1
-        for (let i = currentMinValue; i <= 5; i++) {
-            const optionElement = document.createElement('option');
-            optionElement.textContent = i;
-            optionElement.value = i;
-            maxRating.appendChild(optionElement);
-        }
-        if (parseInt(previousMaxValue) >= currentMinValue) {
-            maxRating.value = previousMaxValue;
-        } 
-    });
+maxRating.addEventListener('change', function() {
+    const previousMinValue = minRating.value;
+    const currentMaxValue = parseInt(maxRating.value);
 
-    // Event listener for menu2
-    maxRating.addEventListener('change', function() {
-        const previousMinValue = minRating.value;
-        const currentMaxValue = parseInt(maxRating.value);
+    minRating.innerHTML = '';
+    const optionElement = document.createElement('option');
+    optionElement.textContent = 'Minimum Rating:';
+    optionElement.value = 0;
+    minRating.appendChild(optionElement);
 
-        minRating.innerHTML = '';
+    for (let i = 0; i <= currentMaxValue; i++) {
         const optionElement = document.createElement('option');
-        optionElement.textContent = 'Minimum Rating:';
-        optionElement.value = 0;
+        optionElement.textContent = i;
+        optionElement.value = i;
         minRating.appendChild(optionElement);
-        // Add new options to menu2 based on selection of menu1
-        for (let i = 0; i <= currentMaxValue; i++) {
-            const optionElement = document.createElement('option');
-            optionElement.textContent = i;
-            optionElement.value = i;
-            minRating.appendChild(optionElement);
-        }
-        if (parseInt(previousMinValue) <= currentMaxValue) {
-            minRating.value = previousMinValue;
-        } 
+    }
+    if (parseInt(previousMinValue) <= currentMaxValue) {
+        minRating.value = previousMinValue;
+    } 
+})
+
+
+//event listeners for button labels
+function toggleRowLabel (isOn, browseClass, rowLabel){
+    const elmnts = document.querySelectorAll(browseClass)
+    const labels = document.querySelectorAll(browseClass + '-label')
+    elmnts.forEach(elmnt =>{
+        elmnt.style['display'] = isOn ? 'block' : 'none'
     })
+    labels.forEach(label =>{
+        label.style['display'] = isOn ? 'block' : 'none'
+    })
+}
+document.querySelectorAll(".row-label").forEach(rowLabel=>{
+    rowLabel.addEventListener("click", () => {
+        rowLabel.classList.toggle("row-label--selected")
+        const isOn = rowLabel.classList.contains('row-label--selected');
+        const className = '.' + rowLabel.textContent
+        toggleRowLabel(isOn, className, rowLabel)
+    })
+})
+
+//event listeners for playcount
+document.querySelectorAll(".playcount").forEach(numInput=>{
+    numInput.addEventListener('input', function(){
+        const inputVal = numInput.value
+        if (inputVal > Number.MAX_SAFE_INTEGER) {numInput.value = Number.MAX_SAFE_INTEGER}
+        numInput.value = inputVal.replace(/[^0-9]/g, '')
+    })
+})
+const minPlaycount = document.getElementById("min-playcount")
+const maxPlaycount = document.getElementById("max-playcount")
+minPlaycount.addEventListener('blur', function(){
+    const minimumCount = parseFloat(minPlaycount.value)
+    const maximumCount = parseFloat(maxPlaycount.value)
+
+    if(minimumCount > maximumCount){
+        minPlaycount.value = maximumCount - 1
+    }
+})
+maxPlaycount.addEventListener('blur', function(){
+    const minimumCount = parseFloat(minPlaycount.value)
+    const maximumCount = parseFloat(maxPlaycount.value)
+
+    if(minimumCount > maximumCount){
+        maxPlaycount.value = minimumCount + 1
+    }
+})
