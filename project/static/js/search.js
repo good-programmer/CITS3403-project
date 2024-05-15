f8c95d8069860a6704061b1169cebf81d0d36846
naetnaetnaet <buluran.nathan@gmail.com>
2024-05-15 18:42:47 +0800

    added 3 maps that allows for values to be taken from input fields to be stored for use to fetch data later, added feature to browse buttons that clears values if browse by <keyname> is not selected, otherwise it will use the previous input which is stored

--- project/static/js/search.js
+++ project/static/js/search.js
@@ -1,7 +1,32 @@
-const minRating = document.getElementById('min-rating');
-const maxRating = document.getElementById('max-rating');
+//url map preparation
+const searchMap = new Map()
+const defaultMap = new Map()
+const storedMap = new Map()
+
+defaultMap.set('query', '');
+defaultMap.set('rating', '0-5');
+defaultMap.set('date', { after: '0000-01-01', to: '9999-01-01' });
+defaultMap.set('completed', 'true');
+defaultMap.set('play_count', '0-999999');
+defaultMap.set('sort_by', 'date');
+defaultMap.set('order', 'desc');
+
+storedMap.set('rating', '0-5');
+storedMap.set('date', { after: '0000-01-01', to: '9999-01-01' });
+storedMap.set('play_count', '0-999999');
+
+function setDefault (keyName){
+    searchMap.set(keyName, defaultMap.get(keyName))
+}
+
+const keyNameList = ['query', 'rating', 'date', 'completed', 'play_count', 'sort_by', 'order']
+for (const keyName of keyNameList){
+    setDefault(keyName)
+}
 
 //event listeners for rating
+const minRating = document.getElementById('min-rating');
+const maxRating = document.getElementById('max-rating');
 minRating.addEventListener('change', function() {
     const currentMinValue = parseInt(minRating.value);
     const previousMaxValue = maxRating.value;
@@ -21,9 +46,11 @@ minRating.addEventListener('change', function() {
     }
     if (parseInt(previousMaxValue) >= currentMinValue) {
         maxRating.value = previousMaxValue;
-    } 
+    }
+    const newValue = String(minRating.value) + '-' + String(maxRating.value)
+    searchMap.set('rating', newValue)
+    console.log(searchMap.get('rating')) 
 });
-
 maxRating.addEventListener('change', function() {
     const previousMinValue = minRating.value;
     const currentMaxValue = parseInt(maxRating.value);
@@ -42,12 +69,14 @@ maxRating.addEventListener('change', function() {
     }
     if (parseInt(previousMinValue) <= currentMaxValue) {
         minRating.value = previousMinValue;
-    } 
+    }
+    const newValue = String(minRating.value) + '-' + String(maxRating.value)
+    searchMap.set('rating', newValue)
+    console.log(searchMap.get('rating'))
 })
 
-
 //event listeners for button labels
-function toggleRowLabel (isOn, browseClass, rowLabel){
+function toggleRowLabel (isOn, browseClass){
     const elmnts = document.querySelectorAll(browseClass)
     const labels = document.querySelectorAll(browseClass + '-label')
     elmnts.forEach(elmnt =>{
@@ -61,16 +90,27 @@ document.querySelectorAll(".row-label").forEach(rowLabel=>{
     rowLabel.addEventListener("click", () => {
         rowLabel.classList.toggle("row-label--selected")
         const isOn = rowLabel.classList.contains('row-label--selected');
-        const className = '.' + rowLabel.textContent
-        toggleRowLabel(isOn, className, rowLabel)
+        const keyName = rowLabel.textContent.replace(" ","_")
+
+        if (!isOn){
+            storedMap.set(keyName, searchMap.get(keyName))
+            setDefault(keyName)
+        }
+        else{
+            searchMap.set(keyName, storedMap.get(keyName))
+        }
+
+        const className = '.' + keyName
+        toggleRowLabel(isOn, className)
+        console.log(searchMap.get(keyName))
     })
 })
 
 //event listeners for playcount
-document.querySelectorAll(".playcount").forEach(numInput=>{
+document.querySelectorAll(".play_count").forEach(numInput=>{
     numInput.addEventListener('input', function(){
         const inputVal = numInput.value
-        if (inputVal > Number.MAX_SAFE_INTEGER) {numInput.value = Number.MAX_SAFE_INTEGER}
+        if (inputVal > 999999) {numInput.value = 999999}
         numInput.value = inputVal.replace(/[^0-9]/g, '')
     })
 })
@@ -83,6 +123,10 @@ minPlaycount.addEventListener('blur', function(){
     if(minimumCount > maximumCount){
         minPlaycount.value = maximumCount - 1
     }
+
+    const newValue = String(minPlaycount.value) + '-' + String(maxPlaycount.value)
+    searchMap.set('play_count', newValue)
+    console.log(searchMap.get('play_count'))
 })
 maxPlaycount.addEventListener('blur', function(){
     const minimumCount = parseFloat(minPlaycount.value)
@@ -91,24 +135,70 @@ maxPlaycount.addEventListener('blur', function(){
     if(minimumCount > maximumCount){
         maxPlaycount.value = minimumCount + 1
     }
+
+    const newValue = String(minPlaycount.value) + '-' + String(maxPlaycount.value)
+    searchMap.set('play_count', newValue)
+    console.log(searchMap.get('play_count'))
 })
 
 //event listeners for date
+function formatDate(date) {
+    const year = date.getFullYear().toString().padStart(4,'0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    return year + '-' + month + '-' + day;
+}
 const minDate = document.getElementById('min-date');
 const maxDate = document.getElementById('max-date');
 minDate.addEventListener('input', function() {
     const minDateVal = new Date(minDate.value);
     const maxDateVal = new Date(maxDate.value);
 
+    if (!maxDate.value) {maxDateVal.setFullYear(9999, 0, 1)}
+    if (!minDate.value) {minDateVal.setFullYear(0, 0, 1)}
+    
     if (maxDateVal < minDateVal) {
         maxDate.valueAsDate = minDateVal;
     }
+
+    searchMap.set('date', { after: formatDate(minDateVal), to: formatDate(maxDateVal) })
+    console.log(searchMap.get('date'))
 });
 maxDate.addEventListener('input', function() {
-    const minDateValue = new Date(minDate.value);
-    const maxDateValue = new Date(maxDate.value);
+    const minDateVal = new Date(minDate.value);
+    const maxDateVal = new Date(maxDate.value);
 
-    if (maxDateValue < minDateValue) {
-        minDate.valueAsDate = maxDateValue;
+    if (!maxDate.value) {maxDateVal.setFullYear(9999, 0, 1)}
+    if (!minDate.value) {minDateVal.setFullYear(0, 0, 1)}
+
+    if (maxDateVal < minDateVal) {
+        minDate.valueAsDate = maxDateVal;
     }
+    searchMap.set('date', { after: formatDate(minDateVal), to: formatDate(maxDateVal) })
+    console.log(searchMap.get('date'))
 });
+
+//event listers for basic-toggle
+document.querySelectorAll(".basic-toggle").forEach(basicToggle=>{
+    basicToggle.addEventListener("click", () => {
+        basicToggle.classList.toggle("basic-toggle--selected")
+    })
+})
+
+const orderButton = document.getElementById("order")
+orderButton.addEventListener("click", () =>{
+    const isDesc = orderButton.classList.contains('basic-toggle--selected');
+    orderButton.textContent = isDesc ? 'Descending' : 'Ascending'
+})
+
+//event listiners for search
+const searchSubmit = document.getElementById("search-trigger")
+const searchInput = document.getElementById("search")
+searchInput.addEventListener("input", e =>{
+    const input = e.target.value
+    searchMap.set('query', input)
+    console.log(searchMap.get("query"))
+})
+searchSubmit.addEventListener("submit", () =>{
+    const query = searchInput.value.lower.trim
+})
